1d008f3b674e3f107aca1d831e56006dbb3291fb
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-03-09 12:57:43 -0500

    Export and import functions

--- app2.js
+++ app2.js
@@ -3,5 +3,13 @@
 // module
 
 import nombreTarea from './tarea.js'
+import {tarea2, tareaCompletada} from './tarea2.js'
+
 
 console.log(nombreTarea)
+
+const tarea1 = tarea2('hacer el curso de UX/UI', 'Alta')
+
+console.log(tarea1)
+
+tareaCompletada()
